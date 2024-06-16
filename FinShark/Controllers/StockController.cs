6552bfad591e1e69f8e7de1e2a069d5c3bb0fd53
Azizjon8800 <azizjon8437@gmail.com>
2024-06-16 23:30:31 +0500

    Add project files.

---
+++ FinShark/Controllers/StockController.cs
@@ -0,0 +1,101 @@
+﻿using FinShark.Data;
+using FinShark.Dtos.Stock;
+using FinShark.Helpers;
+using FinShark.Interfaces;
+using FinShark.Mapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinShark.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : Controller
+    {
+        private readonly IStockRepository _stockRepository;
+        public StockController(IStockRepository stockRepository)
+        {
+            _stockRepository = stockRepository;
+        }
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var stocks = await _stockRepository.GetAllAsync(query);
+            var stockDto = stocks.Select(s => s.ToStockDto()).ToList();
+
+            return Ok(stockDto);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var stock = await _stockRepository.GetByIdAsync(id);
+            
+            if(stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock.ToStockDto());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var stockModel = stockDto.ToStockFromCreateDto();
+            await _stockRepository.CreateAsync(stockModel);
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var stockModel = await _stockRepository.UpdateAsync(id, updateDto);
+              
+            if(stockModel == null)
+            {
+                return NotFound();          
+            }
+
+            return Ok(stockModel.ToStockDto());
+        }
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var stockModel = await _stockRepository.DeleteAsync(id);
+             
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+
+
+            return NoContent();
+        }
+    }
+}
---
+++ FinShark/Migrations/20240616174744_CommentOneToOne.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FinShark.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommentOneToOne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "16186399-5392-48a8-9ba8-9cd160271756");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2728cefd-3c4f-4e35-b2c2-c90f682ff2ce");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AppUserId",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "7ade66e6-bd52-4bb1-9fb7-db8503b79119", null, "User", "USER" },
+                    { "a22c3847-38b9-419a-b5ef-2c6b2fbc1e8b", null, "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AppUserId",
+                table: "Comments",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserId",
+                table: "Comments",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_AppUserId",
+                table: "Comments");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7ade66e6-bd52-4bb1-9fb7-db8503b79119");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a22c3847-38b9-419a-b5ef-2c6b2fbc1e8b");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Comments");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "16186399-5392-48a8-9ba8-9cd160271756", null, "User", "USER" },
+                    { "2728cefd-3c4f-4e35-b2c2-c90f682ff2ce", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
